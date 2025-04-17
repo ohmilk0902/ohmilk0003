c2a2f01ec085eca9144171bd3594885813ee6ae4
ohmilk0902 <ohmilk0902@gmail.com>
2025-04-18 02:37:00 +0800

    Delete form-config.js

--- form-config.js
+++
@@ -1,8 +0,0 @@
-
-window.formConfig = {
-  products: [
-    { name: "姓名貼", maxImages: 12, workingDays: 5 },
-    { name: "底片鑰匙圈", maxImages: 60, workingDays: 3 },
-    { name: "壓克力吊飾", maxImages: 20, workingDays: 4 }
-  ]
-};
